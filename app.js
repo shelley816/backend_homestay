7af2c2d222fc69b03ee5dc5c4fcfe92e3c36e8c3
Shelley <jialing80916@gmail.com>
2025-05-04 23:12:28 +0800

    test backend

---
+++ app.js
@@ -0,0 +1,113 @@
+import fs from 'node:fs/promises';
+
+import bodyParser from 'body-parser';
+import express from 'express';
+
+import xss from "xss";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  next();
+});
+
+app.get('/products', async (req, res) => {
+  const { search, max } = req.query;
+  const productsFileContent = await fs.readFile('./data/products.json');
+  let products = JSON.parse(productsFileContent);
+
+  if (search) {
+    const keyword = search.toLowerCase();
+    products = products.filter((product) => {
+      const name = product.name?.toLowerCase() || "";
+      const desc = product.description?.toLowerCase() || "";
+      return name.includes(keyword) || desc.includes(keyword);
+    });
+  }
+
+  if (max) {
+    products = products.slice(products.length - max, products.length);
+  }
+
+  res.json({ products });
+});
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const productsFileContent = await fs.readFile('./data/products.json');
+  const products = JSON.parse(productsFileContent);
+
+  const product = products.find((product) => product.id === id);
+
+  if (!product) {
+    return res
+      .status(404)
+      .json({ message: `No product found for id: ${id}` });
+  }
+
+  res.json({ product });
+});
+
+app.post('/orders', async (req, res) => {
+  const orderData = req.body.order;
+
+  orderData.customer.name = xss(orderData.customer.name || "");
+  orderData.customer.email = xss(orderData.customer.email || "");
+  orderData.customer.street = xss(orderData.customer.street || "");
+  orderData.customer["postal-code"] = xss(orderData.customer["postal-code"] || "");
+  orderData.customer.city = xss(orderData.customer.city || "");
+  orderData.customer.leaveMessage = xss(orderData.customer.leaveMessage || "");
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  if (orderData === null || orderData.items === null || orderData.items.length === 0) {
+    return res
+      .status(400)
+      .json({ message: 'Missing data.' });
+  }
+
+  if (
+    orderData.customer.email === null ||
+    !orderData.customer.email.includes('@') ||
+    orderData.customer.name === null ||
+    orderData.customer.name.trim() === '' ||
+    orderData.customer.street === null ||
+    orderData.customer.street.trim() === '' ||
+    orderData.customer['postal-code'] === null ||
+    orderData.customer['postal-code'].trim() === '' ||
+    orderData.customer.city === null ||
+    orderData.customer.city.trim() === ''
+  ) {
+    return res.status(400).json({
+      message:
+        'Missing data: Email, name, street, postal code or city is missing.',
+    });
+  }
+
+  const newOrder = {
+    ...orderData,
+    id: (Math.random() * 1000).toString(),
+  };
+  const orders = await fs.readFile('./data/orders.json', 'utf8');
+  const allOrders = JSON.parse(orders);
+  allOrders.push(newOrder);
+  await fs.writeFile('./data/orders.json', JSON.stringify(allOrders));
+  res.status(201).json({ message: 'Order created!' });
+});
+
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
